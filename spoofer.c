4078108da982496d59b05317efe46b464a5febe4
heathknowles <yyashuday13@gmail.com>
2025-01-02 08:16:05 +0000

    Packet Captiure for the IP Spoof

--- spoofer.c
+++ spoofer.c
@@ -6,6 +6,7 @@
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <unistd.h>
+#include <pcap.h>
 
 unsigned short checksum(void *b, int len) {
     unsigned short *buf = b;
@@ -22,14 +23,44 @@ unsigned short checksum(void *b, int len) {
     return result;
 }
 
+void capture_packets(const char *filename) {
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle;
+
+    handle = pcap_open_live("any", BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Could not open device: %s\n", errbuf);
+        exit(EXIT_FAILURE);
+    }
+
+    pcap_dumper_t *dumper = pcap_dump_open(handle, filename);
+    if (dumper == NULL) {
+        fprintf(stderr, "Could not open pcap file: %s\n", pcap_geterr(handle));
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Capturing packets... Press Ctrl+C to stop.\n");
+    pcap_loop(handle, 0, pcap_dump, (unsigned char *)dumper);
+
+    pcap_dump_close(dumper);
+    pcap_close(handle);
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s <source_ip> <dest_ip>\n", argv[0]);
+    if (argc != 5) {
+        printf("Usage: %s <source_ip> <dest_ip> <num_packets> <pcap_filename>\n", argv[0]);
         return -1;
     }
 
     const char *source_ip = argv[1];
     const char *dest_ip = argv[2];
+    int num_packets = atoi(argv[3]);
+    const char *pcap_filename = argv[4];
+
+    if (num_packets <= 0) {
+        fprintf(stderr, "Number of packets must be greater than 0.\n");
+        return -1;
+    }
 
     int sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
     if (sock < 0) {
@@ -53,36 +84,47 @@ int main(int argc, char *argv[]) {
     ip_header->version = 4;
     ip_header->ihl = 5;  // Header length
     ip_header->tos = 0;  // Type of service
-    ip_header->tot_len = sizeof(struct iphdr) + sizeof(struct icmphdr);
+    ip_header->tot_len = htons(sizeof(struct iphdr) + sizeof(struct icmphdr));
     ip_header->id = htons(54321);  // Identification
     ip_header->frag_off = 0;       // Fragment offset
     ip_header->ttl = 64;           // Time-to-live
     ip_header->protocol = IPPROTO_ICMP;  // Protocol
-    ip_header->check = 0;                // Checksum (calculated later)
     ip_header->saddr = inet_addr(source_ip);  // Source IP
     ip_header->daddr = inet_addr(dest_ip);    // Destination IP
 
-    ip_header->check = checksum((unsigned short *)packet, ip_header->tot_len);
-
     icmp_header->type = ICMP_ECHO;  // ICMP Echo Request
     icmp_header->code = 0;         // Code
-    icmp_header->checksum = 0;     // Checksum (calculated later)
     icmp_header->un.echo.id = htons(1);
-    icmp_header->un.echo.sequence = htons(1);
 
-    icmp_header->checksum = checksum((unsigned short *)icmp_header, sizeof(struct icmphdr));
+    if (fork() == 0) {
+        capture_packets(pcap_filename);
+        exit(0);
+    }
 
-    struct sockaddr_in dest;
-    dest.sin_family = AF_INET;
-    dest.sin_addr.s_addr = inet_addr(dest_ip);
+    for (int i = 0; i < num_packets; i++) {
+        icmp_header->un.echo.sequence = htons(i + 1);
+        icmp_header->checksum = 0;
+        icmp_header->checksum = checksum((unsigned short *)icmp_header, sizeof(struct icmphdr));
 
-    if (sendto(sock, packet, ip_header->tot_len, 0, (struct sockaddr *)&dest, sizeof(dest)) < 0) {
-        perror("Packet send failed");
-    } else {
-        printf("Packet sent from %s to %s\n", source_ip, dest_ip);
+        ip_header->check = 0;
+        ip_header->check = checksum((unsigned short *)ip_header, sizeof(struct iphdr));
+
+        struct sockaddr_in dest;
+        dest.sin_family = AF_INET;
+        dest.sin_addr.s_addr = inet_addr(dest_ip);
+
+        if (sendto(sock, packet, ntohs(ip_header->tot_len), 0, (struct sockaddr *)&dest, sizeof(dest)) < 0) {
+            perror("Packet send failed");
+        } else {
+            printf("Packet %d sent from %s to %s\n", i + 1, source_ip, dest_ip);
+        }
+
+        usleep(100000);  // Wait 100ms between packets
     }
 
     close(sock);
+    printf("All packets sent. Capture saved to %s.\n", pcap_filename);
+
     return 0;
 }
 
